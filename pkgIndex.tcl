6382e189538d1e3d6141fa4584bfe02679ef0588
Danilo Chang <ray2501@gmail.com>
2023-12-16 13:37:38 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tcllzf 0.5 \
-    [list load [file join $dir libtcllzf0.5.so] tcllzf]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tcllzf 0.5 \
+	    [list load [file join $dir libtcl9tcllzf0.5.so] [string totitle tcllzf]]
+} else {
+    package ifneeded tcllzf 0.5 \
+	    [list load [file join $dir libtcllzf0.5.so] [string totitle tcllzf]]
+}
